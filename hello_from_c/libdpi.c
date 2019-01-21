5131d90dff04079c8e9515e3d571d1924d4c2a4a
Kaushal Modi <kaushal.modi@gmail.com>
2019-01-20 22:25:29 -0500

    Add hello_from_c

---
+++ hello_from_c/libdpi.c
@@ -0,0 +1,12 @@
+// Time-stamp: <2014-11-17 23:30:12 kmodi>
+
+// Source: http://www.testbench.in/DP_03_IMPORT.html
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <string.h>
+
+void string_sv2c(){
+  printf("Hello from C!\n");
+}
---
+++ hello_from_c/tb.v
@@ -0,0 +1,13 @@
+// Time-stamp: <2014-11-17 23:27:09 kmodi>
+
+// Source: http://www.testbench.in/DP_03_IMPORT.html
+
+program main;
+
+   import "DPI-C" string_sv2c=task string_sv2c();
+
+      initial begin
+         string_sv2c();
+      end
+
+endprogram
