f06660873d3dcda24745623948bd4b94a6ece806
Kaushal Modi <kaushal.modi@gmail.com>
2019-01-21 13:23:42 -0500

    Use withScatchRegion + --gc:regions
    https://irclogs.nim-lang.org/21-01-2019.html#18:04:11

--- testbench_in__DPI_C/08_arrays/libdpi.nim
+++ testbench_in__DPI_C/08_arrays/libdpi.nim
@@ -41,36 +41,36 @@ proc add_lpv(aRef, bRef, cRef: ref svLogicVecVal) {.exportc.} =
 # An array of packed logic is passed as a reference to an array of
 # svLogicVecVal from the SV side.
 proc get_nums(numsRef: ref array[10, svLogicVecVal]) {.exportc.} =
-  # https://irclogs.nim-lang.org/21-01-2019.html#17:52:51
-  GC_disable()
-  echo fmt"packed logic array length = {numsRef[].len}"
-  for i in 0 .. numsRef[].high:
-    numsRef[][i].aval = uint32(i+10)
-    numsRef[][i].bval = 0
-    # The echoing of the array elements causes crash if nim
-    # compilation is done without --gc:none *or* without the above
-    # GC_disable() call.
-    echo fmt"Nim: numsRef[][{i}] = {numsRef[][i]}"
+  # https://irclogs.nim-lang.org/21-01-2019.html#18:04:11
+  withScratchRegion:
+    echo fmt"packed logic array length = {numsRef[].len}"
+    for i in 0 .. numsRef[].high:
+      numsRef[][i].aval = uint32(i+10)
+      numsRef[][i].bval = 0
+      # The echoing of the array elements causes crash if nim
+      # compilation is done without --gc:none *or* without
+      # --gc:regions and this wrapping in withScratchRegion.
+      echo fmt"Nim: numsRef[][{i}] = {numsRef[][i]}"
 
 # Above can be alternatively written as below too.
 proc get_nums2(numsRef: ref array[10, svLogicVecVal]) {.exportc.} =
-  GC_disable()
-  var
-    nums = numsRef[]
-  echo fmt"packed logic array length = {nums.len}"
-  for i in 0 .. nums.high:
-    nums[i].aval = uint32(i+10)
-    nums[i].bval = 0
-    echo fmt"Nim: nums[{i}] = {nums[i]}"
-  numsRef[] = nums
+  withScratchRegion:
+    var
+      nums = numsRef[]
+    echo fmt"packed logic array length = {nums.len}"
+    for i in 0 .. nums.high:
+      nums[i].aval = uint32(i+10)
+      nums[i].bval = 0
+      echo fmt"Nim: nums[{i}] = {nums[i]}"
+    numsRef[] = nums
 
 # Also, it looks like that array of svLogicVecVal can be accessed
 # using "var" too i.e. not by reference. So, surprisingly, below works
 # too.
 proc get_nums3(nums: var array[10, svLogicVecVal]) {.exportc.} =
-  GC_disable()
-  echo fmt"packed logic array length = {nums.len}"
-  for i in 0 .. nums.high:
-    nums[i].aval = uint32(i+10)
-    nums[i].bval = 0
-    echo fmt"Nim: nums[{i}] = {nums[i]}"
+  withScratchRegion:
+    echo fmt"packed logic array length = {nums.len}"
+    for i in 0 .. nums.high:
+      nums[i].aval = uint32(i+10)
+      nums[i].bval = 0
+      echo fmt"Nim: nums[{i}] = {nums[i]}"
