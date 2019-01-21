fd0371f8df40f0dad73650a60db10a41e27043d7
Kaushal Modi <kaushal.modi@gmail.com>
2019-01-21 10:33:38 -0500

    Fix s_vpi_vecval definition
    1. Needed to export the object elements too!
    2. The name of those identifiers need to be the exact same as in the
       struct in the C headers, including the case of the identifiers.

--- svdpi.nim
+++ svdpi.nim
@@ -4,18 +4,20 @@ import os
 # cDisableCaching()
 # cDebug()
 
-# Below manual definition of s_vpi_vecval is a workaround for
-# https://github.com/genotrance/nimterop/issues/47.
-type
-  s_vpi_vecval* {.importc: "s_vpi_vecval".} = object
-    aval: uint32
-    bval: uint32
-
 const
   xlmIncludePath = getEnv("XCELIUM_ROOT") / ".." / "include"
 static:
   doAssert fileExists(xlmIncludePath / "svdpi.h")
   doAssert fileExists(xlmIncludePath / "svdpi_compatibility.h")
+
+cDefine("DPI_COMPATIBILITY_VERSION_1800v2012")
+
+# Below manual definition of s_vpi_vecval is a workaround for
+# https://github.com/genotrance/nimterop/issues/47.
+type
+  s_vpi_vecval* {.importc: "s_vpi_vecval", header: xlmIncludePath / "svdpi.h".} = object
+    aval*: uint32 # we need to export the object elements too!
+    bval*: uint32
 
 cAddSearchDir(xlmIncludePath)
 cImport(cSearchPath("svdpi.h"))
