264cae8414399ee8e0a2b5a9fc3ebad64154b920
Kaushal Modi <kaushal.modi@gmail.com>
2019-01-28 16:46:43 -0500

    Optimize the Nim header wrappers

--- systemverilog_dpi_now/user-interface/draw/EGGX080/eggx.nim
+++ systemverilog_dpi_now/user-interface/draw/EGGX080/eggx.nim
@@ -31,25 +31,30 @@ cPlugin:
 
 const
   headereggx_base0 = hDir / "eggx_base.h"
+  headereggx_color0 = hDir / "eggx_color.h"
 
 # This block must be added before the cImport for which the overrides
 # are happening.
 cOverride:
   # Workaround for tree-sitter not passing varargs info to nimterop:
   #   https://github.com/genotrance/nimterop/issues/76
-  proc winname*(wn: cint; a1: cstring): cint                                                                            {.varargs, importc: "eggx_$1", header: headereggx_base0.}
-  proc newcolor*(a1: cint; a2: cstring)                                                                                 {.varargs, importc: "eggx_$1", header: headereggx_base0.}
-  proc gsetfontset*(a1: cint; a2: cstring): cint                                                                        {.varargs, importc: "eggx_$1", header: headereggx_base0.}
-  proc drawstr*(a1: cint; a2: cfloat; a3: cfloat; a4: cint; a5: cfloat; a6: cstring)                                    {.varargs, importc: "eggx_$1", header: headereggx_base0.}
-  proc saveimg*(a1: cint; a2: cint; a3: cfloat; a4: cfloat; a5: cfloat; a6: cfloat; a7: cstring; a8: cint; a9: cstring) {.varargs, importc: "eggx_$1", header: headereggx_base0.}
-  proc gsetborder*(a1: cint; a2: cint; a3: cstring)                                                                     {.varargs, importc: "eggx_$1", header: headereggx_base0.}
-  proc gsetbgcolor*(a1: cint; a2: cstring)                                                                              {.varargs, importc: "eggx_$1", header: headereggx_base0.}
-  proc gsetinitialborder*(a1: cint; a2: cstring)                                                                        {.varargs, importc: "eggx_$1", header: headereggx_base0.}
-  proc gsetinitialbgcolor*(a1: cstring)                                                                                 {.varargs, importc: "eggx_$1", header: headereggx_base0.}
-  proc gsetinitialparsegeometry*(a: cstring)                                                                            {.varargs, importc: "eggx_$1", header: headereggx_base0.}
+  {.push varargs.}
+  {.push header: headereggx_base0.}
+  proc winname*(wn: cint; a1: cstring): cint                                              {.importc: "eggx_$1".}
+  proc newcolor*(a1: cint; a2: cstring)                                                   {.importc: "eggx_$1".}
+  proc gsetfontset*(a1: cint; a2: cstring): cint                                          {.importc: "eggx_$1".}
+  proc drawstr*(a1: cint; a2, a3: cfloat; a4: cint; a5: cfloat; a6: cstring)              {.importc: "eggx_$1".}
+  proc saveimg*(a1, a2: cint; a3, a4, a5, a6: cfloat; a7: cstring; a8: cint; a9: cstring) {.importc: "eggx_$1".}
+  proc gsetborder*(a1, a2: cint; a3: cstring)                                             {.importc: "eggx_$1".}
+  proc gsetbgcolor*(a1: cint; a2: cstring)                                                {.importc: "eggx_$1".}
+  proc gsetinitialborder*(a1: cint; a2: cstring)                                          {.importc: "eggx_$1".}
+  proc gsetinitialbgcolor*(a1: cstring)                                                   {.importc: "eggx_$1".}
+  proc gsetinitialparsegeometry*(a: cstring)                                              {.importc: "eggx_$1".}
+  {.pop.} # {.push header: headereggx_base0.}
+  {.pop.} # {.push varargs.}
 
 cImport(headereggx_base0)
-cImport(hDir / "eggx_color.h")
+cImport(headereggx_color0)
 
 # Ref: https://gitter.im/nim-lang/Nim?at=5c4f3b76c2dba5382ea13ef8
 #      http://ix.io/1zqR/nim
