60ad510151e51f4cadaaf6ff7ee4ff92020a3de5
Kaushal Modi <kaushal.modi@gmail.com>
2019-02-01 18:26:25 -0500

    Remove unnecessary includes from a C example

--- hello_from_c/libdpi.c
+++ hello_from_c/libdpi.c
@@ -1,11 +1,8 @@
-// Time-stamp: <2014-11-17 23:30:12 kmodi>
+// Time-stamp: <2019-02-01 18:26:20 kmodi>
 
 // Source: http://www.testbench.in/DP_03_IMPORT.html
 
 #include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-#include <string.h>
 
 void string_sv2c(){
   printf("Hello from C!\n");
