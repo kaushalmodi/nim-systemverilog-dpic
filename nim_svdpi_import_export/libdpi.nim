066b1b5824bbba3e2b43c667bc75e915e4547b7b
Kaushal Modi <kaushal.modi@gmail.com>
2019-03-24 14:52:48 -0400

    Use {.discardable.} pragma instead of using discard

--- nim_svdpi_import_export/libdpi.nim
+++ nim_svdpi_import_export/libdpi.nim
@@ -1,12 +1,12 @@
-# Time-stamp: <2019-03-24 14:25:09 kmodi>
+# Time-stamp: <2019-03-24 14:39:18 kmodi>
 # https://verificationacademy.com/resources/technical-papers/dpi-redux-functionality-speed-optimization
 
 import svdpi
 import strformat
 
 # Signature / header lines for the functions/tasks defined in SV.
-proc sv_print_scope(value: cint): cint {.importc.}
-proc sv_consume_time(d: cint): cint {.importc.}
+proc sv_print_scope(value: cint): cint {.importc, discardable.}
+proc sv_consume_time(d: cint): cint {.importc, discardable.}
 
 proc nimAddFunction(a, b: cint; c: ref cint): cint {.exportc.} =
   #                                ^^^ - c is an output on SV side, so it becomes a ref here.
@@ -14,7 +14,7 @@ proc nimAddFunction(a, b: cint; c: ref cint): cint {.exportc.} =
     scopeName = svGetNameFromScope(svGetScope())
   c[] = a + b
   echo fmt"Nim: {scopeName}.nimAddFunction({a}, {b}, {c[]})"
-  discard sv_print_scope(c[])
+  sv_print_scope(c[])
   return 0
 
 proc nimAddTask(a, b: cint; c: ref cint): cint {.exportc.} =
@@ -22,7 +22,7 @@ proc nimAddTask(a, b: cint; c: ref cint): cint {.exportc.} =
     scopeName = svGetNameFromScope(svGetScope())
   c[] = a + b
   echo fmt"Nim: {scopeName}.nimAddTask({a}, {b}, {c[]})"
-  discard sv_consume_time(10)
-  discard sv_consume_time(10)
-  discard sv_print_scope(c[])
+  sv_consume_time(10)
+  sv_consume_time(10)
+  sv_print_scope(c[])
   return 0
