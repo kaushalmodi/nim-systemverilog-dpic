80ee002db57c81c78423489da78490003864c0be
Kaushal Modi <kaushal.modi@gmail.com>
2019-03-25 18:56:34 -0400

    Update template

--- _template/libdpi.nim
+++ _template/libdpi.nim
@@ -1,2 +1,22 @@
-proc hello() {.exportc.} =
-  echo "Hello from Nim!"
+import svdpi
+import strformat
+
+proc FROM_SV_TASK(in1, in2: cint; out1: var cint): cint {.importc, discardable.}
+
+proc FROM_SV_FUNC(in1, in2: cint; out1: var cint) {.importc.}
+
+
+proc TO_SV_TASK(in1, in2: cint; out1: var cint): cint {.exportc.} =
+  echo "Exported to SV task"
+  return 0 # tasks send a return value of 0
+
+proc TO_SV_FUNC(in1, in2: cint; out1: var cint){.exportc.} =
+  echo "Exported to SV function"
+
+
+proc TO_SV_TASK_c(in1, in2: cint; out1: var cint): cint {.exportc.} =
+  FROM_SV_TASK(in1, in2, out1)
+  return 0 # tasks send a return value of 0
+
+proc TO_SV_FUNC_c(in1, in2: cint; out1: var cint){.exportc.} =
+  FROM_SV_FUNC(in1, in2, out1)
--- _template/tb.sv
+++ _template/tb.sv
@@ -1,11 +1,33 @@
-// Time-stamp: <2019-03-25 16:33:50 kmodi>
+// Time-stamp: <2019-03-25 18:56:01 kmodi>
 
 program top;
 
-  import "DPI-C" function void hello();
+  export "DPI-C" task FROM_SV_TASK;
+  export "DPI-C" function FROM_SV_FUNC;
+
+  import "DPI-C" task TO_SV_TASK(input int in1, in2, output int out);
+  import "DPI-C" function void TO_SV_FUNC(input int in1, in2, output int out);
+
+  import "DPI-C" context task TO_SV_TASK_c(input int in1, in2, output int out);
+  import "DPI-C" context function void TO_SV_FUNC_c(input int in1, in2, output int out);
+
+  task FROM_SV_TASK(input int in1, in2, output int out);
+    $display("Exported from SV task to Nim");
+  endtask : FROM_SV_TASK
+
+  function void FROM_SV_FUNC(input int in1, in2, output int out);
+    $display("Exported from SV function to Nim");
+  endfunction : FROM_SV_FUNC
 
   initial begin
-    hello();
+    int temp;
+
+    TO_SV_TASK(1, 2, temp);
+    TO_SV_FUNC(1, 2, temp);
+
+    TO_SV_TASK_c(1, 2, temp);
+    TO_SV_FUNC_c(1, 2, temp);
+
     $finish;
   end
 
