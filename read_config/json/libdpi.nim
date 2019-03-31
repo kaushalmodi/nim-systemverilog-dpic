d0bef7b624de0e37c9ff6777813e9afe299b53df
Kaushal Modi <kaushal.modi@gmail.com>
2019-03-31 17:01:08 -0400

    Add json config reading example

---
+++ read_config/json/libdpi.nim
@@ -0,0 +1,15 @@
+import svdpi
+import strformat, json
+from os import getCurrentDir, `/`
+from sequtils import mapIt
+
+let
+  configFile = getCurrentDir() / "config.json"
+  cfg = json.parseFile(configFile)
+
+proc dump_cfg() {.exportc.} =
+  echo fmt"Config read from {configFile}:"
+  echo cfg.pretty()
+
+# Include the nim code common for reading TOML or JSON config files.
+include "read_config_common.nim"
