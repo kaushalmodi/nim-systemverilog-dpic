4961a36bed2893a53307bd417da7bece99acfea1
Kaushal Modi <kaushal.modi@gmail.com>
2019-04-05 18:03:48 -0400

    Use local copies of the Nim files instead of symlinks
    Else the library building happens using the files in the parent dir of
    the symlink destinations and the results get confusing.

--- struct_from_matlab_nimterop/libdpi.nim
+++ struct_from_matlab_nimterop/libdpi.nim
@@ -1,1 +1,32 @@
-../struct_from_matlab/libdpi.nim
+import svdpi, mean_func_1
+
+proc get_mean_func_1_out(arrPtr: svOpenArrayHandle): OutputObj =
+  let
+    arrSize = svSizeOfArray(arrPtr)
+    arrLen = svLength(arrPtr, 1)
+    uncheckedArrPtr0 = alloc0(arrSize)
+    uncheckedArrPtr1 = cast[ptr UncheckedArray[cint]](uncheckedArrPtr0)
+    dataObjRef = DataObjRef(data: uncheckedArrPtr1)
+    inp1 = InputObjRef(data: dataObjRef,
+                       Len: arrLen)
+    out1: OutputObjRef = new(OutputObj) # initialize
+
+  for i in 0.cint ..< arrLen:
+    let
+      arrElemPtr = cast[ptr cint](svGetArrElemPtr1(arrPtr, i))
+    inp1[].data[].data[i] = arrElemPtr[]
+
+  mean_func_1(inp1, out1)
+  return out1[]
+
+proc get_mean(arrPtr: svOpenArrayHandle): cdouble {.exportc.} =
+  return get_mean_func_1_out(arrPtr).mean
+
+proc get_max(arrPtr: svOpenArrayHandle): cint {.exportc.} =
+  return get_mean_func_1_out(arrPtr).Max
+
+proc get_min(arrPtr: svOpenArrayHandle): cint {.exportc.} =
+  return get_mean_func_1_out(arrPtr).Min
+
+proc get_params(arrPtr: svOpenArrayHandle; params: ref OutputObj) {.exportc.} =
+  params[] = get_mean_func_1_out(arrPtr)
--- struct_from_matlab_nimterop/mean_func_test.nim
+++ struct_from_matlab_nimterop/mean_func_test.nim
@@ -1,1 +1,16 @@
-../struct_from_matlab/mean_func_test.nim
+import mean_func_1
+
+let
+  inputData = [100.cint, 3, 7, 5]
+  dataPtr = cast[ptr UncheckedArray[cint]](unsafeAddr inputData[0])
+
+  dataObjRef = DataObjRef(data: dataPtr)
+
+  inp1 = InputObjRef(data: dataObjRef,
+                     Len: inputData.len.cint)
+  out1: OutputObjRef = new(OutputObj) # initialize
+
+mean_func_1(inp1, out1)
+
+echo inputData
+echo $out1[]
