2285deba8feb81db2fd83e2010d5f96e21559bbc
Kaushal Modi <kaushal.modi@gmail.com>
2019-04-11 10:28:22 -0400

    I like for loops

--- struct_of_struct_from_matlab/mean_func_test.nim
+++ struct_of_struct_from_matlab/mean_func_test.nim
@@ -4,10 +4,13 @@ let
   inputData = [ [632.cint, 68, 87, 73, 22, 63, 20, 04, 88, 85 ],
                 [104.cint, 36, 53, 35, 00, 36, 51, 73, 74, 08 ],
                 [080.cint, 83, 60, 66, 77, 50, 40, 64, 07, 24 ] ]
-  inp1 = [ InputObj(data: inputData[0], len: inputData[0].len.cint),
-           InputObj(data: inputData[1], len: inputData[1].len.cint),
-           InputObj(data: inputData[2], len: inputData[2].len.cint) ]
   out1: OutputObjRef = new(OutputObj) # initialize
+
+var
+  inp1: array[3, InputObj]
+
+for idx, arr in inputData:
+  inp1[idx] = InputObj(data: arr, len: arr.len.cint)
 
 mean_func(inp1, out1)
 
