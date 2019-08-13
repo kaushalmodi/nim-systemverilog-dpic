414796711a26bc805ce0c4f0376d8630a94e008c
Kaushal Modi <kaushal.modi@gmail.com>
2019-08-13 09:14:52 -0400

    Minor refactoring

--- nim_svdpi_import_export/libdpi.nim
+++ nim_svdpi_import_export/libdpi.nim
@@ -1,27 +1,27 @@
-# Time-stamp: <2019-04-15 10:06:44 kmodi>
+# Time-stamp: <2019-08-13 09:13:36 kmodi>
 # https://verificationacademy.com/resources/technical-papers/dpi-redux-functionality-speed-optimization
 
+import std/[strformat]
 import svdpi
-import strformat
 
 # Signature / header lines for the functions/tasks defined in SV.
 proc sv_print_scope(value: cint) {.importc.}
-proc sv_consume_time(d: cint): cint {.importc, discardable.}
+proc sv_consume_time(delay: cint): cint {.importc, discardable.}
 
-proc nimAddFunction(a, b: cint; c: ptr cint): cint {.exportc.} =
-  #                                ^^^ - c is an output on SV side, so it becomes a ptr here.
+proc addFunction(a, b: cint; c: ptr cint): cint {.exportc.} =
+  #                             ^^^ - c is an output on SV side, so it becomes a ptr here.
   let
     scopeName = svGetNameFromScope(svGetScope())
   c[] = a + b
-  echo fmt"Nim: {scopeName}.nimAddFunction({a}, {b}, {c[]})"
+  echo &"Nim: {scopeName}.addFunction({a}, {b}, {c[]})"
   sv_print_scope(c[])
   return 0
 
-proc nimAddTask(a, b: cint; c: ptr cint): cint {.exportc.} =
+proc addTask(a, b: cint; c: ptr cint): cint {.exportc.} =
   let
     scopeName = svGetNameFromScope(svGetScope())
   c[] = a + b
-  echo fmt"Nim: {scopeName}.nimAddTask({a}, {b}, {c[]})"
+  echo &"Nim: {scopeName}.addTask({a}, {b}, {c[]})"
   sv_consume_time(10)
   sv_consume_time(10)
   sv_print_scope(c[])
--- nim_svdpi_import_export/tb.sv
+++ nim_svdpi_import_export/tb.sv
@@ -1,4 +1,4 @@
-// Time-stamp: <2019-03-04 18:07:54 kmodi>
+// Time-stamp: <2019-08-13 09:14:09 kmodi>
 // https://verificationacademy.com/resources/technical-papers/dpi-redux-functionality-speed-optimization
 
 module top();
@@ -6,24 +6,24 @@ module top();
   export "DPI-C" function sv_print_scope;
   export "DPI-C" task sv_consume_time;
 
-  import "DPI-C" context function int nimAddFunction(input int a, input int b, output int c);
-  import "DPI-C" context task nimAddTask (input int a, input int b, output int c);
+  import "DPI-C" context function int addFunction(input int a, input int b, output int c);
+  import "DPI-C" context task addTask (input int a, input int b, output int c);
 
   function int sv_print_scope(input int value);
     $display("  SV: @%2t: sv_print_scope(): %m [value=%0d]", $time, value);
     return value;
   endfunction
 
-  task sv_consume_time(input int d);
-    #d;
+  task sv_consume_time(input int delay);
+    #delay;
   endtask
 
   initial begin
     int c;
     int ret;
     #10;
-    ret = nimAddFunction(10, 20, c);
-    nimAddTask(10, 20, c);
+    ret = addFunction(10, 20, c);
+    addTask(30, 40, c);
     $finish;
   end
 
