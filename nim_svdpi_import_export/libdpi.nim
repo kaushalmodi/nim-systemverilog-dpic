a312967fc77ba8a01fbc9c8162d66c743999c2bb
Kaushal Modi <kaushal.modi@gmail.com>
2019-08-13 09:30:04 -0400

    Refactor

--- nim_svdpi_import_export/libdpi.nim
+++ nim_svdpi_import_export/libdpi.nim
@@ -1,6 +1,3 @@
-# Time-stamp: <2019-08-13 09:13:36 kmodi>
-# https://verificationacademy.com/resources/technical-papers/dpi-redux-functionality-speed-optimization
-
 import std/[strformat]
 import svdpi
 
@@ -8,22 +5,21 @@ import svdpi
 proc sv_print_scope(value: cint) {.importc.}
 proc sv_consume_time(delay: cint): cint {.importc, discardable.}
 
-proc addFunction(a, b: cint; c: ptr cint): cint {.exportc.} =
-  #                             ^^^ - c is an output on SV side, so it becomes a ptr here.
+template add(a, b: cint; c: ptr cint) {.dirty.} =
   let
     scopeName = svGetNameFromScope(svGetScope())
   c[] = a + b
-  echo &"Nim: {scopeName}.addFunction({a}, {b}, {c[]})"
   sv_print_scope(c[])
-  return 0
+
+proc addFunction(a, b: cint; c: ptr cint) {.exportc.} =
+  ## Nim proc mapped to function on SV side.
+  ## c is an output on SV side, so it becomes a ptr here.
+  add(a, b, c)
+  echo &"  Nim: {scopeName}.addFunction({a}, {b}, {c[]})"
 
 proc addTask(a, b: cint; c: ptr cint): cint {.exportc.} =
-  let
-    scopeName = svGetNameFromScope(svGetScope())
-  c[] = a + b
-  echo &"Nim: {scopeName}.addTask({a}, {b}, {c[]})"
-  sv_consume_time(10)
-  sv_consume_time(10)
-  sv_print_scope(c[])
-  sv_consume_time(10)
+  ## Nim proc mapped to task on SV side.
+  add(a, b, c)
+  sv_consume_time(100)
+  echo &"  Nim: {scopeName}.addTask({a}, {b}, {c[]})"
   return 0
--- nim_svdpi_import_export/tb.sv
+++ nim_svdpi_import_export/tb.sv
@@ -1,16 +1,13 @@
-// Time-stamp: <2019-08-13 09:14:09 kmodi>
-// https://verificationacademy.com/resources/technical-papers/dpi-redux-functionality-speed-optimization
-
 module top();
 
   export "DPI-C" function sv_print_scope;
   export "DPI-C" task sv_consume_time;
 
-  import "DPI-C" context function int addFunction(input int a, input int b, output int c);
+  import "DPI-C" context function void addFunction(input int a, input int b, output int c);
   import "DPI-C" context task addTask (input int a, input int b, output int c);
 
   function int sv_print_scope(input int value);
-    $display("  SV: @%2t: sv_print_scope(): %m [value=%0d]", $time, value);
+    $display("SV: @%2t: sv_print_scope(): %m [value=%0d]", $realtime, value);
     return value;
   endfunction
 
@@ -20,10 +17,11 @@ module top();
 
   initial begin
     int c;
-    int ret;
-    #10;
-    ret = addFunction(10, 20, c);
-    addTask(30, 40, c);
+    $timeformat(-9, 0, "ns", 13);
+    #20;
+    addTask(10, 20, c);
+    addFunction(30, 40, c);
+    #3;
     $finish;
   end
 
