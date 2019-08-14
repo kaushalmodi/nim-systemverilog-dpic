7aa89d6760b3046c9f480ba107002b58c7aa3113
Kaushal Modi <kaushal.modi@gmail.com>
2019-08-14 09:33:31 -0400

    Simplify: remove the enable_sticky arg [wip]

--- i_am_here/libdpi.nim
+++ i_am_here/libdpi.nim
@@ -3,18 +3,14 @@ import svdpi
 
 var
   hereCounter: cint = 0
-  enable: cint = 1
 
-proc hereDebug(initVal: cint; enableSticky: cint; realVal: cdouble): cstring {.exportc.} =
-  if enableSticky >= 0:
-    enable = enableSticky
-  if enable > 0:
-    if initVal >= 0:
-      hereCounter = initVal
-    let
-      realStr = if realVal >= 0.0:
-                  &"@{realVal} "
-                else:
-                  ""
-    result = &"{realStr}<{hereCounter}> I am in `{svGetNameFromScope(svGetScope())}'"
-    hereCounter += 1
+proc hereDebug(initVal: cint; realVal: cdouble): cstring {.exportc.} =
+  if initVal >= 0:
+    hereCounter = initVal
+  let
+    realStr = if realVal >= 0.0:
+                &"@{realVal} "
+              else:
+                ""
+  result = &"{realStr}<{hereCounter}> I am in `{svGetNameFromScope(svGetScope())}'"
+  hereCounter += 1
--- i_am_here/tb.sv
+++ i_am_here/tb.sv
@@ -1,4 +1,4 @@
-// Time-stamp: <2019-08-14 09:28:00 kmodi>
+// Time-stamp: <2019-08-14 09:32:47 kmodi>
 
 `timescale 1ns/1ns
 
@@ -10,23 +10,17 @@ package here_pkg;
   //   If negative, the here counter increments from the previous value.
   //   Else, the here counter gets set to the passed value.
   //     The here counter auto-initializes to 0 if not set using init_val.
-  // enable_sticky:
-  //   If negative, the state of "here debug enable" stays the same as before.
-  //   Else if enable_sticky is 0, the here debug state gets disabled.
-  //   Else if enable_sticky is positive, the here debug state gets enabled.
-  //     The enable state auto-initializes to 1 if not set using enable_sticky.
   // real_val:
   //   If negative, this value is not embedded in the string returned by hereDebug.
   //   Else, this value is prefixed to the returned string by hereDebug.
   //     Ideally, if this value is set, it should be set to $realtime.
   import "DPI-C" context hereDebug = function string here_debug_str(input int init_val = -1,
-                                                                    input int enable_sticky = -1,
                                                                     input real real_val = -1.0);
 
-  function void here_debug(input int init_val = -1, int enable_sticky = -1, real real_val = -1.0, string scope = $sformatf("%m"));
+  function void here_debug(input int init_val = -1, real real_val = -1.0, string scope = $sformatf("%m"));
     string str;
     begin
-      str = here_debug_str(init_val, enable_sticky, real_val);
+      str = here_debug_str(init_val, real_val);
       if (str != "") `uvm_info("HERE", $sformatf("%s [%s]", str, scope), UVM_MEDIUM)
     end
   endfunction : here_debug
@@ -45,21 +39,21 @@ module top;
 
   initial begin
     fork
-      here_debug(, , 0.11);      // 0.0 ns (1) enable=1, counter=0                -> @0.11 <0> I am in `top' :HERE:        ==actual-order-of-printing==> @0.11 <0> I am in `top' :HERE:
-      here_debug(, 0, 0.22);     // 0.0 ns    (2) enable=0, counter=0                                                                                    @0.55 <1> I am in `top' :HERE:
-      here_debug(, , 0.33);      // 0.0 ns       (3) enable=0, counter=0                                                                                 @0.44 <2> I am in `top' :HERE:
+      here_debug(, 0.11);
+      here_debug(, 0.22);
+      here_debug(, 0.33);
       begin
         #2;
-        here_debug(, , 0.44);    // 2.0 ns               (6) enable=1, counter=2        -> @0.44 <2> I am in `top' :HERE:
+        here_debug(, 0.44);
       end
       begin
         #1;
-        here_debug(, 1, 0.55);   // 1.0 ns            (5) enable=1, counter=1        -> @0.55 <1> I am in `top' :HERE:
+        here_debug(, 0.55);
       end
-      here_debug(, , 0.66);      // 0.0 ns         (4) enable=0, counter=0
+      here_debug(, 0.66);
     join
 
-    here_debug(,,,$sformatf("%m"));
+    here_debug(,,$sformatf("%m"));
     here_debug();
     here_debug(100);
     here_debug();
@@ -74,13 +68,7 @@ module top;
     here_debug();
     here_debug();
     here_debug(200);
-    here_debug(200, 0); // These will not be printed
-    here_debug(); // These will not be printed
-    here_debug(0); // These will not be printed
-    here_debug(); // These will not be printed
-    here_debug(); // These will not be printed
-    here_debug(); // These will not be printed
-    here_debug(99, 1);
+    here_debug(99);
     here_debug();
     here_debug();
     here_debug();
@@ -104,7 +92,7 @@ virtual class my_base_test extends uvm_test;
   virtual function void build_phase(uvm_phase phase);
     begin
       super.build_phase(phase);
-      here_debug(,,,$sformatf("%m"));
+      here_debug(,,$sformatf("%m"));
     end
   endfunction : build_phase
 endclass : my_base_test
@@ -114,7 +102,7 @@ class my_test extends my_base_test;
 
   function new(string name = "my_test", uvm_component parent = null);
     super.new(name, parent);
-    here_debug(,,,$sformatf("%m"));
+    here_debug(,,$sformatf("%m"));
   endfunction : new
 
   virtual task run_phase(uvm_phase phase);
@@ -124,7 +112,7 @@ class my_test extends my_base_test;
 
       #5;
 
-      here_debug(,,,$sformatf("%m"));
+      here_debug(,,$sformatf("%m"));
 
       `uvm_info("HELLO", "Hello", UVM_MEDIUM)
       #10;
