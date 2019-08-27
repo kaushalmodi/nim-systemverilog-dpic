e76b6d91113ef12fb04dea8b6aae971d8480dd7b
Kaushal Modi <kaushal.modi@gmail.com>
2019-08-27 19:57:45 -0400

    Add example of getting structure from the Nim side

--- struct_elements/libdpi.nim
+++ struct_elements/libdpi.nim
@@ -9,9 +9,18 @@ type
     arrInt: array[Max, cint]
     arrBit: array[Max, byte]
 
-proc print_object(obj: ptr MyObject) {.exportc.} =
+proc print_object(objPtr: ptr MyObject) {.exportc.} =
   echo "Printing the whole object:"
-  echo $obj[]
+  echo $objPtr[]
   echo "Printing the object while looping through each key:"
-  for key, val in obj[].fieldPairs:
+  for key, val in objPtr[].fieldPairs:
     echo "  ", $key, " = ", $val
+
+proc get_object(objPtr: ptr MyObject) {.exportc.} =
+  var
+    obj = MyObject(scalarBit: 1,
+                   scalarReal: 2.3,
+                   scalarInt: 456,
+                   arrInt: [1.cint, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+                   arrBit: [1.byte, 0, 1, 1, 1, 0, 1, 1, 0, 0])
+  objPtr[] = obj
--- struct_elements/tb.sv
+++ struct_elements/tb.sv
@@ -1,4 +1,4 @@
-// Time-stamp: <2019-08-27 19:47:02 kmodi>
+// Time-stamp: <2019-08-27 19:55:33 kmodi>
 
 program top;
 
@@ -14,25 +14,32 @@ program top;
   } my_struct_s;
 
   import "DPI-C" function void print_object(input my_struct_s s);
+  import "DPI-C" function void get_object(output my_struct_s s);
 
   initial begin
-    my_struct_s sa, sb;
+    begin
+      my_struct_s sa, sb;
 
-    sa.scalar_bit = 1;
-    sa.scalar_real = 1.123;
-    sa.scalar_int = 100;
-    sa.arr_int = '{ 51, 58, 64, 53, 74, 54, 35, 27, 01, 38 };
-    sa.arr_bit = '{ 0, 1, 0, 0, 1, 0, 0, 1, 0, 0 };
-    print_object(sa);
+      sa.scalar_bit = 1;
+      sa.scalar_real = 1.123;
+      sa.scalar_int = 100;
+      sa.arr_int = '{ 51, 58, 64, 53, 74, 54, 35, 27, 01, 38 };
+      sa.arr_bit = '{ 0, 1, 0, 0, 1, 0, 0, 1, 0, 0 };
+      print_object(sa);
 
-    sb.scalar_bit = 0;
-    sb.scalar_real = 8.076;
-    sb.scalar_int = 882;
-    sb.arr_int = '{ 44, 58, 22, 65, 55, 81, 56, 30, 25, 04 };
-    sb.arr_bit = '{ 0, 1, 0, 0, 1, 0, 0, 1, 0, 0 };
-    print_object(sb);
-
+      sb.scalar_bit = 0;
+      sb.scalar_real = 8.076;
+      sb.scalar_int = 882;
+      sb.arr_int = '{ 44, 58, 22, 65, 55, 81, 56, 30, 25, 04 };
+      sb.arr_bit = '{ 0, 1, 0, 0, 1, 0, 0, 1, 0, 0 };
+      print_object(sb);
+    end
+    begin
+      my_struct_s s;
+      get_object(s);
+      print_object(s);
+    end
     $finish;
-  end
+  end // initial begin
 
 endprogram : top
