c24e1a3bd20bbf7a03a7da061793ae79faf86515
Kaushal Modi <kaushal.modi@gmail.com>
2019-11-08 16:17:00 -0500

    Use generics to expand chandle example to procs handling real & str

--- chandle/libdpi.nim
+++ chandle/libdpi.nim
@@ -5,8 +5,16 @@ type
   Chandle = pointer
 
 proc adder[T](args: openArray[T]): T =
+  var
+    idx = 0
   for arg in args:
-    result = result + arg
+    when T is cstring:
+      result = cstring($result & $arg)
+      if idx < args.high:
+        result = cstring($result & " ")
+    else:
+      result = result + arg
+    inc(idx)
 
 proc getAdderProcHandle[T](): Chandle =
   ## Return pointer to a "C function type".
@@ -25,3 +33,15 @@ proc getAdderProcHandleInt(): Chandle {.exportc.} =
 
 proc callProcInt(ch: Chandle; chArgsPtr: svOpenArrayHandle): cint {.exportc.} =
   callProc[cint](ch, chArgsPtr)
+
+proc getAdderProcHandleFloat(): Chandle {.exportc.} =
+  getAdderProcHandle[cdouble]()
+
+proc callProcFloat(ch: Chandle; chArgsPtr: svOpenArrayHandle): cdouble {.exportc.} =
+  callProc[cdouble](ch, chArgsPtr)
+
+proc getAdderProcHandleString(): Chandle {.exportc.} =
+  getAdderProcHandle[cstring]()
+
+proc callProcString(ch: Chandle; chArgsPtr: svOpenArrayHandle): cstring {.exportc.} =
+  callProc[cstring](ch, chArgsPtr)
--- chandle/tb.sv
+++ chandle/tb.sv
@@ -1,9 +1,15 @@
-// Time-stamp: <2019-11-08 15:55:48 kmodi>
+// Time-stamp: <2019-11-08 16:16:30 kmodi>
 
 program top;
 
   import "DPI-C" getAdderProcHandleInt = function chandle get_adder_handle_int();
   import "DPI-C" callProcInt = function int call_int(input chandle ch, int args[]);
+
+  import "DPI-C" getAdderProcHandleFloat = function chandle get_adder_handle_real();
+  import "DPI-C" callProcFloat = function real call_real(input chandle ch, real args[]);
+
+  import "DPI-C" getAdderProcHandleString = function chandle get_adder_handle_string();
+  import "DPI-C" callProcString = function string call_string(input chandle ch, string args[]);
 
   initial begin
 
@@ -20,8 +26,32 @@ program top;
       //   xmsim: *E,MEMALC: Memory not allocated for actual array.
       args = '{100, 200, 300, 400, 500};
       sum = call_int(adder_ch, args);
-      $display("Sum of %p = %p", args, sum);
+      $display("Int sum of %p = %p", args, sum);
     end : chandle_to_int_proc
+
+    begin : chandle_to_real_proc
+      chandle adder_ch; // This is the handle to an adder proc implemented in Nim
+      real args[];
+      real sum;
+
+      adder_ch = get_adder_handle_real();
+
+      args = '{4.5, 61.7, 1.44, 48.6, 0.213};
+      sum = call_real(adder_ch, args);
+      $display("Real sum of %p = %p", args, sum);
+    end : chandle_to_real_proc
+
+    begin : chandle_to_string_proc
+      chandle adder_ch; // This is the handle to an adder proc implemented in Nim
+      string args[];
+      string sum;
+
+      adder_ch = get_adder_handle_string();
+
+      args = '{"You", "are", "seeing", "the", "magic", "of", "Nim", "generics!"};
+      sum = call_string(adder_ch, args);
+      $display("String sum of %p = %p", args, sum);
+    end : chandle_to_string_proc
 
     $finish;
   end
