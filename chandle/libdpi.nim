0c1f868bdc99b67e14500647e536d27e3f218f73
Kaushal Modi <kaushal.modi@gmail.com>
2019-11-08 15:51:55 -0500

    Refactor the chandle for generics; prep for the next commits

--- chandle/libdpi.nim
+++ chandle/libdpi.nim
@@ -4,17 +4,24 @@ type
   # Alias Nim type "pointer" to "Chandle" just for better readability.
   Chandle = pointer
 
-proc adder(args: openArray[cint]): cint =
+proc adder[T](args: openArray[T]): T =
   for arg in args:
     result = result + arg
 
-proc getAdderProcHandle(): Chandle {.exportc.} =
+proc getAdderProcHandle[T](): Chandle =
   ## Return pointer to a "C function type".
-  result = cast[Chandle](adder)
+  return cast[Chandle](adder[T])
 
-proc callProc(ch: Chandle; chArgsPtr: svOpenArrayHandle): cint {.exportc.} =
+proc callProc[T](ch: Chandle; chArgsPtr: svOpenArrayHandle): T =
   ## Call the C function pointed by ``ch`` with args referenced by ``chArgsPtr``.
   let
-    procInst = cast[type adder](ch)
-    args = svSVDynArr1ToSeq[cint](chArgsPtr)
+    procInst = cast[type adder[T]](ch)
+    args = svSVDynArr1ToSeq[T](chArgsPtr)
   return procInst(args)
+
+## Exported Procs
+proc getAdderProcHandleInt(): Chandle {.exportc.} =
+  getAdderProcHandle[cint]()
+
+proc callProcInt(ch: Chandle; chArgsPtr: svOpenArrayHandle): cint {.exportc.} =
+  callProc[cint](ch, chArgsPtr)
--- chandle/tb.sv
+++ chandle/tb.sv
@@ -1,27 +1,27 @@
-// Time-stamp: <2019-11-08 15:42:11 kmodi>
+// Time-stamp: <2019-11-08 15:55:48 kmodi>
 
 program top;
 
-  import "DPI-C" getAdderProcHandle = function chandle get_adder_handle();
-
-  // Assume that calling the chandle runs a proc on Nim side that
-  // returns an int.
-  import "DPI-C" callProc = function int call_int(input chandle ch, int args[]);
+  import "DPI-C" getAdderProcHandleInt = function chandle get_adder_handle_int();
+  import "DPI-C" callProcInt = function int call_int(input chandle ch, int args[]);
 
   initial begin
-    chandle adder_proc_handle; // This is the handle to an adder proc implemented in Nim
-    int args[];
-    int sum;
 
-    adder_proc_handle = get_adder_handle();
+    begin : chandle_to_int_proc
+      chandle adder_ch; // This is the handle to an adder proc implemented in Nim
+      int args[];
+      int sum;
 
-    // args = '{};
-    // Above is illegal; args cannot be an empty array.
-    // Above will cause this error:
-    //   xmsim: *E,MEMALC: Memory not allocated for actual array.
-    args = '{100, 200, 300, 400, 500};
-    sum = call_int(adder_proc_handle, args);
-    $display("Sum of %p = %p", args, sum);
+      adder_ch = get_adder_handle_int();
+
+      // args = '{};
+      // Above is illegal; args cannot be an empty array.
+      // Above will cause this error:
+      //   xmsim: *E,MEMALC: Memory not allocated for actual array.
+      args = '{100, 200, 300, 400, 500};
+      sum = call_int(adder_ch, args);
+      $display("Sum of %p = %p", args, sum);
+    end : chandle_to_int_proc
 
     $finish;
   end
