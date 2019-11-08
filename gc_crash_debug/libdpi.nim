a62e9e0019636d2d6c2e87d869f195d23d073c90
Kaushal Modi <kaushal.modi@gmail.com>
2019-11-08 10:43:26 -0500

    Add an example of manual disable/enable of GC

--- gc_crash_debug/libdpi.nim
+++ gc_crash_debug/libdpi.nim
@@ -1,5 +1,10 @@
 proc imported_func(){.exportc.} =
+  # https://gitter.im/nim-lang/Nim?at=5dc584763f4ea333f2c5345d
+  when defined(gcToggle):
+    GC_disable()
   var
     someStr: string
   for i in 0 .. 1000:
     someStr = $i & "aaaaaaaaaaaaaaa"
+  when defined(gcToggle):
+    GC_enable()
