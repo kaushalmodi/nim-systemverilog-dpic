0f4ed98158e1374a5f1426008a88216f778d27bc
Kaushal Modi <kaushal.modi@gmail.com>
2021-05-04 10:34:07 -0400

    DVCon 2021 DPI-C: Add example of recording Nim signals in SV
    This example is almost rewritten from scratch. It highlights Nim
    features like templates.

---
+++ fast_river_of_data_dvcon_2021/f.recording/libdpi.nim
@@ -0,0 +1,79 @@
+# Time-stamp: <2021-05-04 10:36:12 kmodi>
+
+import std/[random]
+import svdpi
+
+var
+  rs = initRand(1) # Init the RNG with some arbitrary fixed seed
+  recordingEnabled = true
+
+type
+  MyObj = object
+    b: svBit
+    l: svLogic
+    i: cint
+    li: clonglong
+
+proc record_an_int(i: cint) {.importc.}
+proc record_my_struct(iPtr: ptr MyObj) {.importc.}
+proc tictoc(times: cint): cint {.importc.}
+
+template withScope(scopeName: untyped, body: untyped) =
+  let
+    newScope = svGetScopeFromName(scopeName.cstring)
+    oldScope = svSetScope(newScope)
+  body
+  discard svSetScope(oldScope)
+
+proc disable_recording() {.exportc, dynlib.} =
+  recordingEnabled = false
+
+proc run_nim_code1(): cint {.exportc, dynlib.} =
+  var
+    k = 0
+    s = MyObj()
+
+  while true:
+    # Update `s'
+    s.b = rs.rand(1).svBit
+    s.l = rs.rand(3).svLogic
+    inc(s.i, rs.rand(2))
+    inc(s.li, rs.rand(10))
+    if s.i > 50:
+      s.i = -50
+    if s.li > 500:
+      s.li = -500
+
+    if not recordingEnabled:
+      break
+    discard tictoc(1) # wait for 1 clock edge
+    withScope "top.u_recorder_1":
+      record_my_struct(addr s) # Record Nim variables
+      record_an_int(k.cint)
+    inc k
+  return 0
+
+proc run_nim_code2(): cint {.exportc, dynlib.} =
+  var
+    k = 0
+    s = MyObj()
+
+  while true:
+    # Update `s'
+    s.b = rs.rand(1).svBit
+    s.l = rs.rand(3).svLogic
+    inc(s.i, rs.rand(5))
+    inc(s.li, rs.rand(100))
+    if s.i > 100:
+      s.i = -100
+    if s.li > 1000:
+      s.li = -1000
+
+    if not recordingEnabled:
+      break
+    discard tictoc(1) # wait for 1 clock edge
+    withScope "top.u_recorder_2":
+      record_my_struct(addr s) # Record Nim variables
+      record_an_int(k.cint)
+    inc k
+  return 0
---
+++ fast_river_of_data_dvcon_2021/f.recording/tb.sv
@@ -0,0 +1,74 @@
+// Time-stamp: <2021-05-04 10:27:53 kmodi>
+
+`timescale 1ns/1ns
+
+interface recorder();
+
+  typedef struct {
+    bit b;
+    logic l;
+    int i;
+    longint li;
+  } my_struct_t;
+
+  int an_int;
+  my_struct_t s;
+
+  export "DPI-C" function record_an_int;
+  export "DPI-C" function record_my_struct;
+
+  function void record_an_int(input int i);
+    an_int = i;
+  endfunction : record_an_int
+
+  function void record_my_struct(input my_struct_t i);
+    s = i;
+  endfunction : record_my_struct
+
+endinterface : recorder
+
+module top();
+
+  import "DPI-C" context task run_nim_code1();
+  import "DPI-C" context task run_nim_code2();
+  import "DPI-C" function void disable_recording();
+  export "DPI-C" task tictoc;
+
+  reg clk;
+
+  recorder u_recorder_1();
+  recorder u_recorder_2();
+
+  task tictoc(input int times);
+    repeat (times) @(posedge clk);
+  endtask : tictoc
+
+  initial begin
+    clk = 0;
+  end
+  always #1ns clk = !clk;
+
+  initial begin
+    $timeformat(-9, 3, "ns");  // units, precision, suffix
+    $shm_open("waves.shm");
+    $shm_probe("ACTM"); // "M" is needed to plot the unpacked arrays like "bit [7:0] unpacked_bytes [4];"
+  end
+
+  initial begin
+    fork
+      // thread # 1
+      run_nim_code1();
+
+      // thread # 2
+      run_nim_code2();
+
+      // thread # 3
+      begin
+        #1_000ns;
+        disable_recording();
+        $finish();
+      end
+    join
+  end // initial begin
+
+endmodule : top
