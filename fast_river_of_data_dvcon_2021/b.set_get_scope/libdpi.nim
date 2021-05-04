b00904eb9bce11ebe0bb0fbf158f8c167b6784df
Kaushal Modi <kaushal.modi@gmail.com>
2021-05-04 17:21:51 -0400

    Fix svGetNameFromScope issue; its ret val needed to be copied to Nim
    If a C API has return type of "const char*", the C implementation owns
    the memory location for that function's return. Always copy those
    return values to a Nim string otherwise the C API can delete/overwrite
    that memory location.
    
    Ref: https://forum.nim-lang.org/t/7924#50456

--- fast_river_of_data_dvcon_2021/b.set_get_scope/libdpi.nim
+++ fast_river_of_data_dvcon_2021/b.set_get_scope/libdpi.nim
@@ -1,4 +1,4 @@
-# Time-stamp: <2021-05-03 22:22:11 kmodi>
+# Time-stamp: <2021-05-04 17:21:29 kmodi>
 
 import std/[strformat]
 import svdpi
@@ -8,59 +8,28 @@ proc f_scopetest_sv() {.importc.}
 proc switchToScope(newScope: svScope) =
   let
     prevScope = svSetScope(newScope)
-  # See below note on why the below line is commented out.
-  # echo &"f_scopetest_nim: Switched from {svGetNameFromScope(prevScope)} to {svGetNameFromScope(newScope)}"
+  echo &"f_scopetest_nim: Switched from {svGetNameFromScope(prevScope)} to {svGetNameFromScope(newScope)}"
   f_scopetest_sv()
 
 proc f_scopetest_nim() {.exportc, dynlib.} =
   var
     scope = svGetScope()
   let
-    origScopeName = svGetNameFromScope(scope)
+    # That $ is important! As the C impl of svGetNameFromScope owns
+    # the returned C string memory location (because its return type
+    # is const char*), we need to copy that to Nim string if we want
+    # to keep that copy, otherwise, each call to svGenNameFromScope
+    # will keep on overwriting the variable as the C impl saves that
+    # function's return value to the same location --
+    # https://forum.nim-lang.org/t/7924#50456
+    origScopeName = $svGetNameFromScope(scope)
 
   switchToScope(scope)
-  echo &"origScopeName = {origScopeName}"
 
   # Switch to the other_module scope
   scope = svGetScopeFromName("top.u_other_module".cstring)
   switchToScope(scope)
-  echo &"origScopeName = {origScopeName}"
 
   # Go back to the original scope
   scope = svGetScopeFromName(origScopeName)
   switchToScope(scope)
-  echo &"origScopeName = {origScopeName}"
-
-# Mon May 03 22:09:22 EDT 2021 - kmodi
-# NOTE:
-#   With line 12 uncommented, we see the immutable `origScopeName' change its value
-#   after the scope switches to top.u_other_module, and the scope still remains
-#   top.u_other_module after the last switch, which is wrong!
-#
-#   xcelium> run
-#   f_scopetest_nim: Switched from top to top
-#   In top.f_scopetest_sv
-#
-#   origScopeName = top
-#   f_scopetest_nim: Switched from top to top.u_other_module
-#   In top.u_other_module.f_scopetest_sv
-#
-#   origScopeName = top.u_other_module
-#   f_scopetest_nim: Switched from top.u_other_module to top.u_other_module
-#   In top.u_other_module.f_scopetest_sv
-#
-#   origScopeName = top.u_other_module
-#
-# But if line 12 is commented out, the final scope is correct! It doesn't make sense ..
-# The calls to `svGetNameFromScope' in that echo statement are messing up things.
-#
-#   xcelium> run
-#   In top.f_scopetest_sv
-#
-#   origScopeName = top
-#   In top.u_other_module.f_scopetest_sv
-#
-#   origScopeName = top
-#   In top.f_scopetest_sv
-#
-#   origScopeName = top
