c72533f5a053a66120074bac684b0c2b2e4aaddc
Kaushal Modi <kaushal.modi@gmail.com>
2022-09-05 14:47:56 -0400

    feat: Demo the same json->struct example using Nim std libs
    In this commit, the Nim std libs json and jsonutils are used to do the
    same JSON string -> Nim object mapping done using the jsony library in
    the previous commit.
    
    The json+jsonutils combo is a bit more picky about the matching of the
    Nim object element names and JSON key names, but it eventually works
    the same way.
    
    - To run the example using the std libs, run "make".
    - To run the example using jsony, run "make jsony".

--- json_to_struct/libdpi.nim
+++ json_to_struct/libdpi.nim
@@ -1,23 +1,46 @@
-import jsony
+when defined(jsony):
+  import jsony
+else:
+  import std/[json, jsonutils]
 
 const
   numElems = 3
 
-type
-  MyObject = object
-    scalarBit: bool
-    scalarReal: cdouble
-    scalarInt: cint
-    arrInt: array[numElems, cint]
+when defined(jsony):
+  # jsony is able to match snake_case keys in the JSON with camelCase
+  # elements in the Nim objects.
+  type
+    MyObject = object
+      scalarBit: bool
+      scalarReal: cdouble
+      scalarInt: cint
+      arrInt: array[numElems, cint]
 
-  MyObject2 = object
-    arrObj: array[numElems, MyObject]
+    MyObject2 = object
+      arrObj: array[numElems, MyObject]
 
-proc getObjectFromJsonFile[T](jsonFile: string): T =
-  return jsonFile.readFile().fromJson(T)
+  proc getObjectFromJsonFile[T](jsonFile: string): T =
+    return jsonFile.readFile().fromJson(T)
+
+else:
+  # jsonutils.jsonTo requires the JSON keys to match exactly with the
+  # object element names.
+  type
+    MyObject = object
+      scalar_bit: bool
+      scalar_real: cdouble
+      scalar_int: cint
+      arr_int: array[numElems, cint]
+
+    MyObject2 = object
+      arr_obj: array[numElems, MyObject]
+
+  proc getObjectFromJsonFile[T](jsonFile: string): T =
+    return jsonFile.readFile().parseJson().jsonTo(T)
 
 proc populateObjectFromJson(jsonFile: cstring; objPtr: ptr MyObject) {.exportc, dynlib.} =
   objPtr[] = getObjectFromJsonFile[MyObject]($jsonFile)
+  #                                          ^ The $ converts cstring to Nim string.
 
 proc populateObject2FromJson(jsonFile: cstring; objPtr: ptr MyObject2) {.exportc, dynlib.} =
   objPtr[] = getObjectFromJsonFile[MyObject2]($jsonFile)
